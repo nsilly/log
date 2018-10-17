6e104c920bc0a14bfc6297cb7b6fa3107343499a
Hieupv <hieupv@codersvn.com>
2018-10-17 19:19:41 +0700

    remove console log

--- src/app/Services/StackTrace.js
+++ src/app/Services/StackTrace.js
@@ -1,14 +1,8 @@
 export class StackTrace {
-  constructor() {
-    console.log('test============');
-  }
   getStack() {
-    console.log('=====GET=====');
-
     return this.stack;
   }
   setStack(stack) {
-    console.log('=====SET=====');
     this.stack = stack;
     return this;
   }
